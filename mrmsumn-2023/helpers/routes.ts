2d1c17d977d296384183d333aee4f7af7b9c52d3
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-02-19 18:26:18 +0700

    add : auth middleware

---
+++ mrmsumn-2023/helpers/routes.ts
@@ -0,0 +1,5 @@
+const protectedRoutes = [
+    '/test'
+];
+
+export default protectedRoutes;
--- mrmsumn-2023/pages/api/auth/login.ts
+++ mrmsumn-2023/pages/api/auth/login.ts
@@ -46,8 +46,16 @@ export default async function handler(
             email : user.email
         }
 
-        jwt.sign(payload, process.env.JWT_SIGNATURE, {expiresIn : process.env.JWT_LIFETIME}, 
+        jwt.sign(payload, process.env.JWT_SIGNATURE, {expiresIn : process.env.JWT_LIFETIME + 'd'}, 
             (err : any , token : any) => {
+
+                if (err) {
+                    return res.status(200).json({
+                        status : "FAILED",
+                        code : 403, 
+                        message : "Failed to navigate to requested page"
+                    })                    
+                }
 
                 return res.status(200).json({
                     status : "SUCCESS",
@@ -55,6 +63,7 @@ export default async function handler(
                     data : {user, token}
                 })
             });     
+            
         return; 
         
     } else {
