71acb6fad5d15780800ef83b8165a0508e90b440
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-04-09 19:29:33 +0700

    add : production version - hastinapura regist base API

--- mrmsumn-2023/helpers/routes.ts
+++ mrmsumn-2023/helpers/routes.ts
@@ -1,13 +1,14 @@
-const protectedRoutes = [
+const protectedRoutes : any = [
     '/test'
 ];
 
-const publicRoutes = [
+const publicRoutes : any = [
     '/',
     '/aboutus',
     '/faq',
     '/recruitment',
-    '/loginwisanggeniadmin'
+    '/loginwisanggeniadmin',
+    '/form-regis'
 
 ];
 
