29adadd1037992a7b88af24030e46c1fe25ee173
KoongHiap <yapkng@gmail.com>
2023-04-13 19:47:35 +0700

    fix: dedikasi

--- mrmsumn-2023/components/FromRecrutmentElement/LoadingView.tsx
+++ mrmsumn-2023/components/FromRecrutmentElement/LoadingView.tsx
@@ -1,4 +1,4 @@
-import { Flex, Img } from "@chakra-ui/react";
+import { Flex, Img, Text, Box } from "@chakra-ui/react";
 import styles from "@/styles/element.module.css";
 
 const LoadingView = () => {
@@ -13,20 +13,35 @@ const LoadingView = () => {
       justifyContent={"center"}
       background={"blackAlpha.800"}
       position={"fixed"}>
-      <Img
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDir={"column"}
+        width={"100%"}
         top="50%"
         left={"50%"}
         transform={"translate(-50%, -50%)"}
-        position={"fixed"}
-        w={{ base: "40%", md: "20%" }}
-        src="/Assets/Hastinapura/hastinapura.png"
-      />
-      <Flex
-        border={"16px solid black"}
-        borderTop={"16px solid #c28824"}
-        width={{ base: "40vw", md: "20vw" }}
-        height={{ base: "40vw", md: "20vw" }}
-        className={styles.loader}></Flex>
+        position={"fixed"}>
+        <Img
+          w={{ base: "40%", md: "20%" }}
+          src="/Assets/Hastinapura/hastinapura.png"
+        />
+        <Flex
+          border={"16px solid black"}
+          borderTop={"16px solid #c28824"}
+          width={{ base: "40vw", md: "20vw" }}
+          height={{ base: "40vw", md: "20vw" }}
+          className={styles.loader}></Flex>
+        <Text
+          top="130%"
+          position={"fixed"}
+          fontWeight="bold"
+          color="#c28824"
+          fontFamily="TrajanPro-Bold"
+          fontSize={{ base: "1.8rem", md: "2rem" }}>
+          Memproses data, harap tunggu sebentar
+        </Text>
+      </Flex>
     </Flex>
   );
 };
