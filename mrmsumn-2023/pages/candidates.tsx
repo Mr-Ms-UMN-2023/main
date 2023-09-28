015f4befcb2699d0f5b5f6c9155861520fba604f
KoongHiap <98584656+Koongh@users.noreply.github.com>
2023-09-29 02:19:29 +0700

    add: latihan folder

--- mrmsumn-2023/pages/candidates.tsx
+++ mrmsumn-2023/pages/candidates.tsx
@@ -10,12 +10,16 @@ import { useState } from "react";
 const Candidates = () => {
   const [desktop] = useMediaQuery("(min-width: 1081px)");
   const [show, setShow] = useState(false);
-  
+
   return (
     <Box minH={"100vh"} minW={"100vW"}>
       <Navbar />
-      <CandidatesIntro desktop={desktop} setShow={setShow}/>
-      {desktop ? <CandidatesDesktop show={show} /> : <CandidatesMobile show={show} />}
+      {/* <CandidatesIntro desktop={desktop} setShow={setShow}/> */}
+      {desktop ? (
+        <CandidatesDesktop show={true} />
+      ) : (
+        <CandidatesMobile show={true} />
+      )}
     </Box>
   );
 };
